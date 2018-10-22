b59b420a0a76e05aa288b612d26af4e68fb366d0
Sireejaa16 <44079294+Sireejaa16@users.noreply.github.com>
2018-10-22 21:45:35 +0530

    Input accepted via user
    slight modification

--- Recursion/decimalToBinary.c
+++ Recursion/decimalToBinary.c
@@ -1,12 +1,21 @@
+//Suppose a positive integer is entered through the keyboard, write a function to find the binary equivalent of this number using recursion.
 #include<stdio.h>
+int BinaryConversion(int n)                                             //Function to compute the binary equivalent of input number
+{
+   if(n==0)
+           return 0;
+   else 
+	   return (n%2)+10*BinaryConversion(n/2);                        //Recursive Call
 
-void decToBi(int n){
-  if(n==0)
-    return;
-  decToBi(n/2);
-  printf("%d", n%2);
 }
 
-int main(){
-  decToBi(13);
+void main()
+{
+    int n,num;
+    printf("Enter the number whose binary equivalentis to be found ");  //Input the number whose binary equivalent is to be found      
+    scanf("%d",&n);
+    num=BinaryConversion(n);
+    printf("\n The binary equivalent of the input number is : %d",num); //Displaying the binary equivalent
+
 }
+
